64314b19ed82fad0fb8c524c9eeed98b364bee5e
A6du1codes <100400182+A6du1codes@users.noreply.github.com>
2022-04-02 19:29:47 +0530

    Add files via upload
    First Commit

---
+++ Login.js
@@ -0,0 +1,6 @@
+function fnValidatephno(Phoneno){
+    document.getElementById("BtnOTP").style.display = "none";
+   document.getElementById("OTPText").style.display = "";
+   document.getElementById("LoginBtn").style.display = "";
+}
+
